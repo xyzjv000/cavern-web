c30471cb8cf54c1ca9accd92f5d6dc5dabf47876
jesvir <xyzjv000@gmail.com>
2024-05-02 19:17:40 +0800

    Password UI Web

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,24 +5,21 @@ import React from "react";
 
 const page = () => {
   const data = [
-    { icon: 'icon1.png', name: 'Item 1', count: 10, length: 20 },
-    { icon: 'icon2.png', name: 'Item 2', count: 15, length: 25 },
-    { icon: 'icon3.png', name: 'Item 3', count: 20, length: 30 },
+    { icon: 'bank', name: 'Apple', count: 10, length: 20 },
+    { icon: 'apple', name: 'Android', count: 15, length: 25 },
+    { icon: 'card-account-details-outline', name: 'Linux', count: 20, length: 30 },
   ];
   
-  console.log(data);
   
   return (
     <Card className="bg-white w-full justify-start">
       <Typography variant="p" className="text-primary font-semibold">
         Your saved passwords
       </Typography>
-      <Websites />
-      <Websites />
-      <Websites />
-      <Websites />
-      <Websites />
-      <Websites />
+      {data.map((item, index) => {
+        return <Websites icon={item.icon} name={item.name} count={item.count} length={data.length} index={index} key={`account-icon-${index}`} />
+      })}
+      
     </Card>
   );
 };
--- components/Websites.jsx
+++ components/Websites.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import Icon from "@mdi/react";
+import * as mdiIcons from "@mdi/js";
+const Websites = ({ icon, name, count, length, index }) => {
+  function convertIconNameToCamelCase(name) {
+    return (
+      "mdi" +
+      name
+        .split("-")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join("")
+    );
+  }
+  // convert name to mdi react format
+  const camelCaseIconName = convertIconNameToCamelCase(icon);
 
-const Websites = ({icon, name, count, length}) => {
   return (
-    <div className="border-b-2 py-4 px-6 flex justify-between items-center">
+    <div
+      className={`${
+        index != length - 1 ? "border-b-2" : ""
+      } py-4 px-6 flex justify-between items-center`}
+    >
       <div className="flex flex-row gap-4 justify-center items-center">
-        <p>1</p>
-        <p className="text-2xl font-semibold text-gray-600">Apple</p>
+        <Icon path={mdiIcons[camelCaseIconName]} size={2} color="#555555" />
+        <p className="text-2xl font-semibold text-[#555555]">{name}</p>
       </div>
       <div className="flex flex-col justify-center items-center">
-        <p className="font-extrabold text-3xl text-primary">12</p>
+        <p className="font-extrabold text-3xl text-primary">{count}</p>
         <p className="font-bold text-sm text-gray-500">Accounts</p>
       </div>
     </div>
